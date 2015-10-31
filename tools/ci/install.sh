500f8a1d8e98d440d50718f886703b5dffc0280e
Éloi Rivard <azmeuk@gmail.com>
2015-10-31 17:05:29 +0100

    Do not build ogre tests and tools

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -5,7 +5,7 @@ if [ -z "$(apt-cache search "libogre-$OGRE_VERSION-dev")" ]; then
     cd ogre
     mkdir build
     cd build
-    cmake ..
+    cmake .. -DOGRE_BUILD_SAMPLES=0 -DOGRE_BUILD_TESTS=0 -DOGRE_BUILD_TOOLS=0
     make -j 4
     $SUDO make install
     cd ../..
